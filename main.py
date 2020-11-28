9d73378e70ae2d5028463be4afea7728be74e590
Dandung Pratidina <dan.dung44@gmail.com>
2020-11-28 13:11:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+time_left = 90 - int(age)
+x = 365*time_left
+y = 52*time_left
+z = 12*time_left
+
+print(f"You have {x} days, {y} weeks, and {z} months left.")
+
+
+
+
+
+
